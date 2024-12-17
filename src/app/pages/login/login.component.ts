d4f058a5541f6b906bb265fdf016e4d0c4a66631
victormanuelvalencia <vvalenciaa.1@icloud.com>
2024-12-17 04:12:22 -0500

    Login y almacenamiento de token

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { SecurityService } from 'src/app/services/security.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+export class LoginComponent {
+  theUser: User;
 
-  ngOnInit() {
+  constructor(private service: SecurityService) {
+    this.theUser = {
+      email: '',
+      password: '',
+    };
   }
-  ngOnDestroy() {
+
+  // Método para realizar el login
+  login() {
+    this.service.login(this.theUser).subscribe({
+      next: (data) => {
+        console.log("RESPUESTA " + JSON.stringify(data));
+        this.service.saveSession(data);
+      },
+      error: (error) => {
+        console.log(error)
+        Swal.fire("Autenticación inválida", "Usuario o contraseña inválida", "error");
+      }
+    })
   }
 
 }
+
+
+
+
